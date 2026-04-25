12236314ecc27bbfd454428822133424c4d51210
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2026-04-25 00:23:58 -0700

    Favicon generated

--- src/app/layout.js
+++ src/app/layout.js
@@ -49,6 +49,11 @@ export default function RootLayout({ children }) {
       className={`${ptSans.variable} ${arimo.className} h-full antialiased`}
     >
       <head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta name="robots" content="index, follow" />
       </head>
       <body className="min-h-full flex flex-col">
--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,10 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <div className="container mx-auto">
-        <h1 className="text-3xl text-center font-bold">Latest Khyber Pakhtunkhwa Jobs 2026</h1>
-        <p className="text-3xl text-center">This  is the description of the job!</p>
+        <h1 className="text-3xl text-center font-bold">Latest Jobs in Pakistan 2026</h1>
+        <p>
+          Find the latest jobs in Pakistan 2026 with daily updates on government, private, and online opportunities. Jobsinkpk helps you explore verified job listings, apply easily, and stay ahead in your career journey.
+        </p>
       </div>
     </main>
   );
