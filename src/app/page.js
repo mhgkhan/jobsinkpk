b4c3df1c3a5457623e21dad3d1b87d92243d096f
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2026-04-29 23:56:09 -0700

    feat: implement job fetching logic and update job listings display

--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,91 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+
+
+const fetchJobs = async () => {
+  let jobsObj = {};
+  try {
+    const request = await fetch(`https://jobsinkpkbackend.vercel.app/publicaccess/jobs`, {
+      method: "GET",
+      headers: { "content-type": "application/json" }
+    })
+
+    if (!request.ok) {
+      jobsObj.error = true;
+      jobsObj.success = false;
+      jobsObj.message = request.statusText
+      return jobsObj;
+    }
+
+    const response = await request.json();
+    if (response.success) {
+      jobsObj.success = true;
+      jobsObj.error = false;
+      jobsObj.message = "Jobs fetched"
+      jobsObj.data = response.data;
+    }
+
+    else {
+      jobsObj.success = false;
+      jobsObj.error = true;
+      jobsObj.message = response.message;
+    }
+
+
+  } catch (error) {
+    jobsObj.success = false;
+    jobsObj.error = true;
+    jobsObj.message = error.message;
+  } finally {
+    return jobsObj;
+  }
+}
+
+
+export default async function Home() {
+
+  const allJobs = await fetchJobs();
+
+
+
   return (
     <main className="min-h-screen">
       <section className="hero">
 
-        <div className="container mx-auto py-20">
+        <div className="container mx-auto md:py-20 py-10">
           <h1 className="my-2 text-3xl text-left font-bold md:w-[50%] w-full">Latest Jobs in Pakistan 2026</h1>
           <p className="my-2 text-lg md:w-[50%] w-full">
             Find the latest jobs in Pakistan 2026 with daily updates on government, private, and online opportunities. Jobsinkpk helps you explore verified job listings, apply easily, and stay ahead in your career journey.
           </p>
         </div>
       </section>
-      <section className="my-5 bg-gray-100">
+      <section className="md:my-5 my-2">
         <div className="container mx-auto">
           <h2 className="text-2xl font-bold"> Today Latest Jobs </h2>
           <table className="my-5 w-full border border-1 border-gray-300">
             <thead>
               <tr className="my-1 border-b border-gray-400 rounded-md">
-                <th className="w-[5%] py-2 px-1 border-r border-gray-400 text-left">S.No</th>
-                <th className="w-[70%] py-2 px-1 border-r border-gray-400 text-left">Title</th>
-                <th className="w-[15%] py-2 px-1 border-r border-gray-400 text-left">Last Date</th>
-                <th className="w-[10%] py-2 px-1 border-r border-gray-400 text-left">Job Details </th>
+                <th className=" py-2 px-1 border-r border-gray-400 text-left">S.No</th>
+                <th className="md:w-[50%] w-autoo py-2 px-1 border-r border-gray-400 text-left">Title</th>
+                <th className=" py-2 px-1 border-r border-gray-400 text-left">Last Date</th>
+                <th className=" py-2 px-1 border-r border-gray-400 text-left">Job Details </th>
               </tr>
             </thead>
             <tbody>
-              {Array.from([0, 1, 2, 3, 4, 5]).map((ele, ind) => {
-                return <tr className="my-1 border-b border-gray-400 rounded-md">
-                  <th className="w-[5%] py-2 px-1 border-r border-gray-400 text-left">{ind + 1}</th>
-                  <th className="w-[70%] py-2 px-1 border-r border-gray-400 text-left">This is the title of this post </th>
-                  <th className="w-[15%] py-2 px-1 border-r border-gray-400 text-left">01/01/2026</th>
-                  <th className="w-[10%] py-2 px-1 border-r border-gray-400 text-left"> <Link href="/" className="w-full bg-blue-500 p-2 rounded-lg text-white font-bold">Job Details</Link> </th>
-                </tr>
-              })}
+              {
+                allJobs.error ? <tr> <td colSpan={4} className="text-red-600 text-lg text-center my-5"> {allJobs.message} </td></tr> : allJobs.data.length < 1 ?
+                  <tr>
+                    <td colSpan={4} className="text-red-600 text-lg text-center my-5"> {"No Jobs found"} </td>
+                  </tr> : allJobs?.data?.map((ele, ind) => {
+                    return <tr key={ind} className="my-1 border-b border-gray-400 rounded-md">
+                      <th className="py-2 px-1 border-r border-gray-400 text-left">{ind + 1}</th>
+                      <th className="md:w-[50%] w-autoo py-2 px-1 border-r border-gray-400 text-left">{ele.jobTitle}</th>
+                      <th className="py-2 px-1 border-r border-gray-400 text-left">{ele.expiryDate}</th>
+                      <th className="py-2 px-1 border-r border-gray-400 text-left"> <Link href="/" className="w-full text-blue-600 p-2 rounded-lg font-bold">Job Details</Link> </th>
+                    </tr>
+                  }
+                  )}
             </tbody>
           </table>
         </div>
