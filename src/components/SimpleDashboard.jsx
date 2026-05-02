7082004e81ee9eaac1d24760fc74e6aea2464241
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2026-05-02 01:23:05 -0700

    added + in visitors

--- src/components/SimpleDashboard.jsx
+++ src/components/SimpleDashboard.jsx
@@ -13,7 +13,7 @@ const SimpleDashboard = ({visitors,jobs}) => {
         <div className='simpledashbboard md:w-[40%] w-full flex items-center justify-center gap-2 md:flex-col flex-row'>
 
             <div className="block-dev w-full h-auto p-2 bg-green-100 rounded-md flex items-center justify-center gap-2 flex-col">
-                <span className='md:text-3xl text-xl  font-bold'>{visitors}</span>
+                <span className='md:text-3xl text-xl  font-bold'>{visitors}+</span>
                 <span className='md:text-2xl md:my-3 my-1 text-lg  '>Visitors </span>
             </div>
 
